4491a4864e663312484a532c654fa31ecb597e3f
Anwesh43 <anweshthecool0@gmail.com>
2020-01-18 17:06:48 +0530

    adding View in activity and enabling fullscreen

--- app/src/main/java/com/anwesh/uiprojects/linkedbarstepdivideview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbarstepdivideview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedbarstepdivideview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.barstepdivideview.BarStepDivideView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BarStepDivideView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
