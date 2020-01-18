f51c6156f78f7bfaca84965cc99b941b3e02af85
Anwesh43 <anweshthecool0@gmail.com>
2020-01-18 16:45:21 +0530

    Adding BarStepDivideView class

--- barstepdivideview/src/main/java/com/anwesh/uiprojects/barstepdivideview/BarStepDivideView.kt
+++ barstepdivideview/src/main/java/com/anwesh/uiprojects/barstepdivideview/BarStepDivideView.kt
@@ -56,3 +56,23 @@ fun Canvas.drawBSDNode(i : Int, scale : Float, paint : Paint) {
     drawStepDividerBars(w, scale, gap, paint)
     restore()
 }
+
+
+class BarStepDivideView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+}
