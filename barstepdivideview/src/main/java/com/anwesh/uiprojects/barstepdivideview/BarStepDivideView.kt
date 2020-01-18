70b02769f8bda41f9e0e7e6f2778ab3de4f38cc1
Anwesh43 <anweshthecool0@gmail.com>
2020-01-18 16:31:50 +0530

    adding constants and libraries

--- barstepdivideview/src/main/java/com/anwesh/uiprojects/barstepdivideview/BarStepDivideView.kt
+++ barstepdivideview/src/main/java/com/anwesh/uiprojects/barstepdivideview/BarStepDivideView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.barstepdivideview
 /**
  * Created by anweshmishra on 18/01/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val bars : Int = 3
+val scGap : Float = 0.02f / bars
+val delay : Long = 20
+val sizeFactor : Float = 2f
+val foreColor : Int = Color.parseColor("#BDBDBD")
+val backColor : Int = Color.parseColor("#BDBDBD")
